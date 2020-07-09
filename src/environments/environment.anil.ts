40eeaca5b96ade530357baf43b8d1671a7ee5dba
Anil Kumar <anilkumar.dhiman1@gmail.com>
2020-07-09 14:54:02 +0530

    dynamic student detail with token id access to url is set.

--- src/environments/environment.anil.ts
+++ src/environments/environment.anil.ts
@@ -2,7 +2,7 @@
 export const environment = {
     production: false,
     envName: 'anil',
-    apiEndpoint: 'http://localhost/bawes/payroll/payroll/staff/web/v1',
+    apiEndpoint: 'http://localhost/bawes/studenthub/studenthub/staff/web/v1',
     environmentName: 'Anil Local Machine',
     // s3Domain: 'pogi-uploads-dev-server',
     serviceWorker: false
