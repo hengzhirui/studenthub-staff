7f6557c2b5f588df49ac8330241290f46b1c3cbe
Anil Kumar <anilkumar.dhiman1@gmail.com>
2020-08-06 20:49:16 +0530

    [Finishes #174161500] create resume issue

--- src/environments/environment.anil.ts
+++ src/environments/environment.anil.ts
@@ -4,8 +4,9 @@ export const environment = {
   envName: 'anil',
   apiEndpoint: 'http://localhost/bawes/studenthub/studenthub/staff/web/v1',
   algoliaCandidateIndex: 'anil_candidate_public',
-  algoliaCacheDuration: 5 * 60 * 1000, //5 min in millisecond
+  algoliaCacheDuration: 5 * 60 * 1000, // 5 min in millisecond
   permanentBucketUrl: 'https://studenthub-uploads-dev-server.s3.amazonaws.com/photos/',
+  urlResume: 'https://studenthub-uploads-dev-server.s3.amazonaws.com/candidate-resume/',
   cloudinaryUrl: 'https://res.cloudinary.com/studenthub/image/upload/c_thumb,w_200,h_200,g_face/v1596525812/candidate-photo/',
   environmentName: 'Anil Local Machine',
   serviceWorker: false
--- src/environments/environment.dev-withoutsentry.ts
+++ src/environments/environment.dev-withoutsentry.ts
@@ -2,10 +2,11 @@ export const environment = {
   production: false,
   envName: 'dev-withoutsentry',
   apiEndpoint: 'https://staff.api.dev.studenthub.co/v1',
-  permanentBucketUrl: "https://studenthub-uploads-dev-server.s3.amazonaws.com/photos/",
+  permanentBucketUrl: 'https://studenthub-uploads-dev-server.s3.amazonaws.com/photos/',
+  urlResume: 'https://studenthub-uploads-dev-server.s3.amazonaws.com/candidate-resume/',
   cloudinaryUrl: 'https://res.cloudinary.com/studenthub/image/upload/c_thumb,w_200,h_200,g_face/v1596525812/candidate-photo/',
   algoliaCandidateIndex: 'dev_candidate_public',
-  algoliaCacheDuration: 5 * 60 * 1000, //5 min in millisecond
+  algoliaCacheDuration: 5 * 60 * 1000, // 5 min in millisecond
   environmentName: 'Dev Server without sentry',
   serviceWorker: true
 };
