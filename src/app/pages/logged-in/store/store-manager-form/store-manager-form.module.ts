9e033c25beee585dcf98ba713c5e821d44c93a20
Krushn Kathrecha <kathrechakrushn@gmail.com>
2020-10-05 20:40:19 +0530

    ability to assign store manager

--- src/app/pages/logged-in/store/store-manager-form/store-manager-form.module.ts
+++ src/app/pages/logged-in/store/store-manager-form/store-manager-form.module.ts
@@ -7,12 +7,14 @@ import { IonicModule } from '@ionic/angular';
 import { StoreManagerFormPageRoutingModule } from './store-manager-form-routing.module';
 
 import { StoreManagerFormPage } from './store-manager-form.page';
+import { LoadingModalModule } from 'src/app/components/loading-modal/loading-modal.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    LoadingModalModule,
     StoreManagerFormPageRoutingModule
   ],
   declarations: [StoreManagerFormPage]
