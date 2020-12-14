9d2952f457ee9953b212ce5bbd3aa225140f3609
Anil Kumar <anilkumar.dhiman1@gmail.com>
2020-12-14 16:46:08 +0530

    [Finishes #176132375,#176133817] store assign issue resolved

--- src/app/pages/logged-in/company/company-view/company-view.page.ts
+++ src/app/pages/logged-in/company/company-view/company-view.page.ts
@@ -160,13 +160,16 @@ export class CompanyViewPage implements OnInit {
   async loadData(silent = false) {
 
     setTimeout(_ => {
-      this.loading = (!silent)
       this.companyStatus = !!(this.company && this.company.company_status);
       this.followup = !!(this.company && this.company.company_followup);
     }, 500);
 
+    setTimeout(_ => {
+      this.loading = (!silent);
+    });
+
     if (!this.company) {
-      this.company = new Company;
+      this.company = new Company();
       this.company.company_id = this.company_id;
     }
 
@@ -175,7 +178,6 @@ export class CompanyViewPage implements OnInit {
       this.loading = false;
       this.deleting = false;
       this.updating = false;
-
       this.company = response;
 
       setTimeout(_ => {
@@ -323,10 +325,10 @@ export class CompanyViewPage implements OnInit {
       this.updating = false;
     });
   }
-  
+
   /**
-   * retrun type name from mime type 
-   * @param file_type 
+   * retrun type name from mime type
+   * @param file_type
    */
   getFileType(file_type) {
     const types = file_type.split('/');
