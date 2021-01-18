eb14ba58d97aba75aea390f2549b02c2f51a7035
Anil Kumar <anilkumar.dhiman1@gmail.com>
2021-01-18 14:12:14 +0530

    [Finishes #176395848] backen api integrated

--- src/app/models/company-contact.ts
+++ src/app/models/company-contact.ts
@@ -4,7 +4,7 @@ export class CompanyContact {
     company_contact_uuid: string;
     contact_uuid: string;
     company_id: number;
-    role: number;
+    role: any;
     created_at: string;
     updated_at: string;
     created_by: string;
