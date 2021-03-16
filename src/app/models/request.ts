c753db5f499e3ab0d5695299669e7c93d586bfc0
Krushn Kathrecha <kathrechakrushn@gmail.com>
2021-03-16 19:25:31 +0530

    model updated

--- src/app/models/request.ts
+++ src/app/models/request.ts
@@ -2,6 +2,7 @@ import { Staff } from './staff';
 import { CompanyContact } from './company-contact';
 import { Company } from './company';
 import { Note } from './note';
+import { Contact } from './contact';
 
 
 export class Request {
@@ -24,7 +25,7 @@ export class Request {
     request_updated_datetime: string;
     requestCreatedBy: Staff;
     requestUpdatedBy: Staff;
-    contact: CompanyContact;
+    contact: Contact;
     company: Company;
     lastActivity: Note;
 }
