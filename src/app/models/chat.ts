d8930cf8776c6f81430bd83c65be8ef44253489d
Er. Krushn Kathrecha <10800870+krushn@users.noreply.github.com>
2024-09-02 19:07:59 +0530

    chat service

---
+++ src/app/models/chat.ts
@@ -0,0 +1,28 @@
+import { Contact } from "./contact";
+import { Company } from "./company";
+import { Staff } from "./staff";
+import { Store } from "./store";
+import { ChatMessage } from "./chat-message";
+
+export class Chat {
+    chat_uuid: string;
+    candidate_id: number;
+    company_id: number;
+    parent_company_id: number;
+    store_id: number;
+    contact_uuid: string;
+    staff_id: number;
+    created_at: string;
+    updated_at: string;
+
+    staffUnreadCount: number;
+    candidateUnreadCount: number;
+    contactUnreadCount: number;
+
+    contact: Contact;
+    company: Company;
+    parentCompany: Company;
+    staff: Staff;
+    store: Store;
+    chatMessages: ChatMessage[];
+}
---
+++ src/app/providers/logged-in/chat.service.ts
@@ -0,0 +1,80 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+//services
+import { AuthHttpService } from './authhttp.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+
+  private _endpoint = '/candidates';
+
+  constructor(private _authhttp: AuthHttpService) { }
+
+  /**
+   * return work history
+   * @param candidate
+   */
+  list(page: number = 1, filters: any): Observable<any> {
+    let url = this._endpoint + '?page=' + page + '&expand=staff,contact,candidate,company,store';
+
+    if (filters.company_id) {
+      url += "&company_id=" + filters.company_id;
+    }
+    
+    if (filters.store_id) {
+      url += "&store_id=" + filters.store_id;
+    }
+
+    if (filters.staff_id) {
+      url += "&staff_id=" + filters.staff_id;
+    }
+
+    return this._authhttp.get(url, true);
+  }
+
+  messages(chat_uuid: string, page: number = 1, last_index: number = null): Observable<any> {
+    let url = this._endpoint + '/messages/' + chat_uuid + "?page=" + page;
+
+    if (last_index) {
+      url += "&last_index=" + last_index;
+    }
+    
+    return this._authhttp.get(url, true);
+  }
+
+  newMessages(chat_uuid: string, last_index: number = null): Observable<any> {
+    let url = this._endpoint + '/new-messages/' + chat_uuid;
+
+    if (last_index) {
+      url += "?last_index=" + last_index;
+    }
+    
+    return this._authhttp.get(url);
+  }
+
+  unreadCount(): Observable<any> {
+    let url = this._endpoint + '/unread-count'
+    return this._authhttp.get(url);
+  }
+
+  view(chat_uuid: string): Observable<any> {
+    let url = this._endpoint + '/' + chat_uuid + "?expand=staff,contact,company,store"
+    return this._authhttp.get(url);
+  }
+
+  sendMessage(chat_uuid: string, message: string): Observable<any> {
+    let url = this._endpoint + '/send-message'
+    return this._authhttp.post(url, {
+      chat_uuid,
+      message
+    });
+  }
+ 
+  markRead(chat_uuid: string): Observable<any> {
+    let url = this._endpoint + '/mark-read/' + chat_uuid;
+    return this._authhttp.patch(url, {});
+  }
+}
