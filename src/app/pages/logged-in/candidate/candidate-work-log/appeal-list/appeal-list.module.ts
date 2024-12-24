41a024f720f40844a86266ec5f3c51aa68864113
Er. Krushn Kathrecha <10800870+krushn@users.noreply.github.com>
2024-12-24 18:39:40 +0530

    hotfix

--- src/app/pages/logged-in/candidate/candidate-work-log/appeal-list/appeal-list.module.ts
+++ src/app/pages/logged-in/candidate/candidate-work-log/appeal-list/appeal-list.module.ts
@@ -9,6 +9,7 @@ import { AppealListPageRoutingModule } from './appeal-list-routing.module';
 import { AppealListPage } from './appeal-list.page';
 import { AppealFilterComponent } from 'src/app/components/appeal-filter/appeal-filter.component';
 import { DatePopupModule } from 'src/app/components/date-popup/date-popup.module';
+import { NoItemsModule } from 'src/app/components/no-items/no-items.module';
 
 @NgModule({
   imports: [
@@ -16,7 +17,8 @@ import { DatePopupModule } from 'src/app/components/date-popup/date-popup.module
     FormsModule,
     IonicModule,
     AppealListPageRoutingModule,
-    DatePopupModule
+    DatePopupModule,
+    NoItemsModule
   ],
   declarations: [AppealListPage, AppealFilterComponent]
 })
